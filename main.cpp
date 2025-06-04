048d04ef6575761a7a32585a7dd6041b8c39f32a
Phan Khoi <phanhuynhminhkhoi@gmail.com>
2025-06-04 15:37:19 +0700

    segment tree

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,15 @@ bool isPrime(int x)
         if (x % i == 0)
             return false;
     return true;
+}
+
+void build(int id, int l, int r, vector<int> &a, vector<int> &t)
+{
+    if (l == r) return void(t[id] = a[l]);
+    int mid = (l + r) / 2;
+    build(id*2, l, mid, a, t);
+    build(id*2 + 1, mid + 1, r, a, t);
+    t[id] = t[id*2] + t[id*2 + 1];
 }
 
 int main()
